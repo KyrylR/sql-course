d13c5d216dff9f05e2748a01d14d7e44249cdb7b
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-24 20:29:04 +0300

    Finish UNION section

--- Analysis Multiple Tables with Joins/UNION.sql
+++ Analysis Multiple Tables with Joins/UNION.sql
@@ -1,1 +1,21 @@
 -- UNION examples
+SELECT 'advisor' AS type,
+       first_name,
+       last_name
+FROM advisor
+UNION
+SELECT 'investor' AS type,
+       first_name,
+       last_name
+FROM investor;
+
+-- Assignment
+SELECT 'advisor' AS type,
+       first_name,
+       last_name
+FROM advisor
+UNION
+SELECT 'staff member' AS type,
+       first_name,
+       last_name
+FROM staff
