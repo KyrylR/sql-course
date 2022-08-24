a14761a5e56674cb6620b4d85b58272fe2fd7107
Kyrylo Riabov <kyryl.ryabov@gmail.com>
2022-08-24 15:11:14 +0300

    Finish group by section

--- Single Table Analysis(STA) Part 1/GROUP_BY.sql
+++ Single Table Analysis(STA) Part 1/GROUP_BY.sql
@@ -7,3 +7,43 @@ GROUP BY
     rating;
 
 /* Nice! */
+
+-- Assignment +
+SELECT
+    rental_duration,
+    COUNT(title) AS films_with_this_rental_duration
+FROM film
+GROUP BY
+    rental_duration;
+
+-- Multiple Group By
+SELECT
+    rental_duration,
+    rating,
+    COUNT(film_id) as count_of_films
+FROM film
+GROUP BY
+    rental_duration,
+    rating;
+
+-- Aggregate functions
+SELECT
+    rating,
+    COUNT(film_id) AS count_of_films,
+    MIN(length) AS shortest_film,
+    MAX(length) AS longest_film,
+    AVG(length) AS avarage_length_of_film,
+    SUM(length) AS total
+FROM film
+GROUP BY rating;
+
+-- Assignment +
+SELECT
+    replacement_cost,
+    COUNT(film_id) AS count_of_films,
+    MIN(rental_rate) AS cheapest_rental,
+    MAX(rental_rate) AS most_expansive_rental,
+    AVG(rental_rate) AS avarage_rental
+FROM film
+GROUP BY
+    replacement_cost;
